663975fcda667694e521e7a66d6472d3e97557b0
DumeGit <nino.dumicic@gmail.com>
2024-06-03 20:54:39 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,31 +1,165 @@
+import os
 import shutil
-
+import zipfile
+import xml.etree.ElementTree as ET
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-import xml.etree.ElementTree as ET
-import zipfile
-import os
 
 app = Flask(__name__)
 CORS(app)
-entities = {}
-links = {}  # Dictionary to store topic information
-associations = {}  # List to store associations
-FOLDER_IGNORE_LIST = (".DS_Store", ".git", ".venv", "__pycache__", ".idea")
+
+FOLDER_IGNORE_LIST = {".DS_Store", ".git", ".venv", "__pycache__", ".idea"}
+
+class Entity:
+    def __init__(self, id, label, image="", description=""):
+        self.id = id
+        self.label = label
+        self.image = image
+        self.description = description
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'label': self.label,
+            'image': self.image,
+            'description': self.description
+        }
+
+class Link:
+    def __init__(self, id, label, image="", description=""):
+        self.id = id
+        self.label = label
+        self.image = image
+        self.description = description
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'label': self.label,
+            'image': self.image,
+            'description': self.description
+        }
+
+class Association:
+    def __init__(self, id, from_id, to_id, label):
+        self.id = id
+        self.from_id = from_id
+        self.to_id = to_id
+        self.label = label
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'from_id': self.from_id,
+            'to_id': self.to_id,
+            'label': self.label
+        }
+
+class EntityStore:
+    def __init__(self):
+        self.entities = {}
+        self.links = {}
+        self.associations = {}
+        self.decision_tree = {}
+
+    def parse_xtm_file(self, file_path):
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+
+        for topic in root.findall(".//{http://www.topicmaps.org/xtm/1.0/}topic"):
+            self._parse_topic(topic)
+        for association in root.findall(".//{http://www.topicmaps.org/xtm/1.0/}association"):
+            self._parse_association(association)
+
+    def _parse_topic(self, topic):
+        topic_id = topic.get("id")
+        topic_type = topic.find(".//{http://www.topicmaps.org/xtm/1.0/}instanceOf") \
+            .find(".//{http://www.topicmaps.org/xtm/1.0/}subjectIndicatorRef") \
+            .attrib['{http://www.w3.org/1999/xlink}href'].split('#')[-1]
+        base_name = topic.find(".//{http://www.topicmaps.org/xtm/1.0/}baseNameString").text
+        image_name, description = "", ""
+
+        if topic_type == 'linkingPhrase':
+            self.links[topic_id] = Link(topic_id, base_name, image_name, description)
+        else:
+            for occurrence in topic.findall(".//{http://www.topicmaps.org/xtm/1.0/}occurrence"):
+                file_path = occurrence.find(".//{http://www.topicmaps.org/xtm/1.0/}resourceRef") \
+                    .attrib['{http://www.w3.org/1999/xlink}href']
+                if file_path.startswith('file'):
+                    _, file_extension = os.path.splitext(file_path)
+                    folder_name, file_name = file_path.split('/./')[1].split('/')
+                    if file_extension.lower() in ('.png', '.jpg', '.jpeg'):
+                        image_name = os.path.join(folder_name, 'images', file_name)
+                    elif file_extension.lower() in ('.txt', '.htm'):
+                        with open(os.path.join(folder_name, 'texts', file_name), 'r') as file:
+                            description = file.read()
+            self.entities[topic_id] = Entity(topic_id, base_name, image_name, description)
+
+    def _parse_occurrence(self, file_path):
+        _, file_extension = os.path.splitext(file_path)
+        folder_name, file_name = file_path.split('/./')[1].split('/')
+        image_name = ""
+        text = ""
+        if file_extension.lower() in ('.png', '.jpg', '.jpeg'):
+            image_name = os.path.join(folder_name, 'images', file_name)
+        elif file_extension.lower() in ('.txt', '.htm'):
+            with open(os.path.join(folder_name, 'texts', file_name), 'r') as file:
+                text = file.read()
+        return image_name, text
+
+
+    def _parse_association(self, association):
+        link_id = association.find(".//{http://www.topicmaps.org/xtm/1.0/}instanceOf") \
+            .find("{http://www.topicmaps.org/xtm/1.0/}topicRef") \
+            .attrib['{http://www.w3.org/1999/xlink}href'].split('#')[-1]
+        from_id = association.find(".//{http://www.topicmaps.org/xtm/1.0/}member[1]/{http://www.topicmaps.org/xtm/1.0/}topicRef") \
+            .attrib['{http://www.w3.org/1999/xlink}href'].split('#')[-1]
+        to_id = association.find(".//{http://www.topicmaps.org/xtm/1.0/}member[2]/{http://www.topicmaps.org/xtm/1.0/}topicRef") \
+            .attrib['{http://www.w3.org/1999/xlink}href'].split('#')[-1]
+        self.associations[link_id] = Association(link_id, from_id, to_id, self.links[link_id].label)
+
+    def build_decision_tree(self):
+        for assoc in self.associations.values():
+            if assoc.from_id not in self.decision_tree:
+                self.decision_tree[assoc.from_id] = {}
+            self.decision_tree[assoc.from_id][assoc.id] = assoc.to_id
+
+    def find_root_node(self):
+        target_nodes = {assoc.to_id for assoc in self.associations.values()}
+        for entity_id in self.entities:
+            if entity_id not in target_nodes:
+                children_list = [
+                    {
+                        "question": self.entities[item[1]].to_dict(),
+                        "answer": self.links[item[0]].to_dict()
+                    }
+                    for item in list(self.decision_tree.get(entity_id, {}).items())
+                ]
+                return {
+                    "root": self.entities[entity_id].to_dict(),
+                    "children": children_list
+                }
+        return None
+
+
+entity_store = EntityStore()
 
 
 @app.route("/get_children", methods=["GET"])
 def get_children():
     try:
         node = request.args.get("node")
-        children_list = [{"question": {'id': item[1], 'label': entities[item[1]].get('label')},
-                          "answer": {'id': item[0], 'label': links[item[0]].get('label'),
-                                     'image': links[item[0]].get('image')}} for item in
-                         list(decision_tree.get(node, {}).items())]
-        return jsonify(
-            {"root": {'id': node, 'label': entities.get(node).get('label'), 'image': entities.get(node).get('image'),
-                      'description': entities.get(node).get('description')},
-             "children": children_list})
+        children_list = [
+            {
+                "question": entity_store.entities[item[1]].to_dict(),
+                "answer": entity_store.links[item[0]].to_dict()
+            }
+            for item in list(entity_store.decision_tree.get(node, {}).items())
+        ]
+        return jsonify({
+            "root": entity_store.entities.get(node).to_dict(),
+            "children": children_list
+        })
     except Exception as e:
         return jsonify({"error": str(e)})
 
@@ -39,46 +173,36 @@ def get_image():
 @app.route('/tree', methods=["GET"])
 def start_tree():
     folder = request.args.get("name").strip()
-    file = folder + ".xml"
-    entities, links = parse_xtm_file(folder + '/' + file)
-    global decision_tree
-    decision_tree = build_decision_tree(links)
-    return jsonify(find_root_node(entities, links))
+    file = f"{folder}.xml"
+    entity_store.parse_xtm_file(os.path.join(folder, file))
+    entity_store.build_decision_tree()
+    return jsonify(entity_store.find_root_node())
 
 
 @app.route('/trees', methods=["GET"])
 def get_trees():
     folder_objects = []
-
     for item in os.listdir():
         if os.path.isdir(item) and item not in FOLDER_IGNORE_LIST:
             description = 'No description available'
             description_file_path = os.path.join(item, 'description.txt')
-            # Check if the description.txt file exists in the directory
             if os.path.isfile(description_file_path):
                 with open(description_file_path, 'r') as file:
                     description = file.read().strip()
-
-            # Create a dictionary object representing the folder
-            folder_object = {
-                'name': item,
-                'description': description
-            }
-            folder_objects.append(folder_object)
-    return folder_objects
+            folder_objects.append({'name': item, 'description': description})
+    return jsonify(folder_objects)
 
 
 @app.route("/load_triads", methods=["POST"])
 def load_triads():
     try:
         uploaded_file = request.files["file"]
-        if uploaded_file.filename != "":
+        if uploaded_file.filename:
             xml_file = extract_files(uploaded_file)
             xml_file_folder = xml_file.split('.xml')[0]
-            entities, links = parse_xtm_file(xml_file_folder + '/' + xml_file)
-            global decision_tree
-            decision_tree = build_decision_tree(links)
-            return jsonify(find_root_node(entities, links))
+            entity_store.parse_xtm_file(os.path.join(xml_file_folder, xml_file))
+            entity_store.build_decision_tree()
+            return jsonify(entity_store.find_root_node())
         else:
             return jsonify({"error": "No file uploaded."})
     except Exception as e:
@@ -88,125 +212,39 @@ def load_triads():
 def extract_files(file):
     with zipfile.ZipFile(file, 'r') as zip_ref:
         xml_files = [f for f in zip_ref.namelist() if f.endswith('.xml')]
-        txt_files = [f for f in zip_ref.namelist() if f.endswith('.txt')]
+        description_file = next((f for f in zip_ref.namelist() if f.endswith('description.txt')), None)
 
-        if not xml_files and not txt_files:
-            return 'No XML or TXT files found in the zip'
+        if not xml_files:
+            return 'No XML files found in the zip'
 
-        xml_filename = xml_files[0] if xml_files else None
+        xml_filename = xml_files[0]
+        folder_name = xml_filename.split('.')[0]
 
-        folder_name = xml_filename.split('.xml')[0]
+        zip_ref.extract(xml_filename, folder_name)
+        if description_file:
+            zip_ref.extract(description_file, folder_name)
 
-        if xml_filename:
-            zip_ref.extract(xml_filename, folder_name)
-
-        images_folder = folder_name + '/images'
+        images_folder = os.path.join(folder_name, 'images')
+        texts_folder = os.path.join(folder_name, 'texts')
         os.makedirs(images_folder, exist_ok=True)
-
-        texts_folder = folder_name + '/texts'
         os.makedirs(texts_folder, exist_ok=True)
 
         for file_item in zip_ref.namelist():
             if file_item.endswith(('.png', '.jpg', '.jpeg', '.txt', '.htm')):
-                if file_item.endswith(('.png', '.jpg', '.jpeg')):
-                    extracted_path = zip_ref.extract(file_item, images_folder)
-                    file_name = os.path.basename(file_item)
-                    new_path = os.path.join(images_folder, file_name)
-                elif file_item.endswith(('.txt', '.htm')):
-                    extracted_path = zip_ref.extract(file_item, texts_folder)
-                    file_name = os.path.basename(file_item)
-                    new_path = os.path.join(texts_folder, file_name)
+                extracted_path = zip_ref.extract(file_item, images_folder if file_item.endswith(('.png', '.jpg', '.jpeg')) else texts_folder)
+                new_path = os.path.join(images_folder if file_item.endswith(('.png', '.jpg', '.jpeg')) else texts_folder, os.path.basename(file_item))
                 shutil.move(extracted_path, new_path)
-                intermediate_dir = os.path.dirname(extracted_path)
-                while intermediate_dir and intermediate_dir != images_folder and intermediate_dir != texts_folder:
-                    try:
-                        os.rmdir(intermediate_dir)
-                    except OSError:
-                        break
-                    intermediate_dir = os.path.dirname(intermediate_dir)
+                clean_up_empty_directories(os.path.dirname(extracted_path), images_folder, texts_folder)
 
         return xml_filename
 
-
-def parse_xtm_file(file_path):
-    """
-    Parses an XTM file and extracts relevant information.
-    Assumes the XTM file follows the structure provided in your example.
-    """
-    tree = ET.parse(file_path)
-    root = tree.getroot()
-
-    for topic in root.findall(".//{http://www.topicmaps.org/xtm/1.0/}topic"):
-        topic_id = topic.get("id")
-        topic_type = topic.find(".//{http://www.topicmaps.org/xtm/1.0/}instanceOf").find(
-            ".//{http://www.topicmaps.org/xtm/1.0/}subjectIndicatorRef").attrib[
-            '{http://www.w3.org/1999/xlink}href'].split('#')[-1]
-        base_name = topic.find(".//{http://www.topicmaps.org/xtm/1.0/}baseNameString").text
-        image_name = ""
-        description = ""
-        if topic_type == 'linkingPhrase':
-            links[topic_id] = {"label": base_name, "image": image_name, "description": description}
-        else:
-            for occurrence in topic.findall(".//{http://www.topicmaps.org/xtm/1.0/}occurrence"):
-                if occurrence is not None:
-                    file_path = occurrence.find(".//{http://www.topicmaps.org/xtm/1.0/}resourceRef").attrib[
-                        '{http://www.w3.org/1999/xlink}href']
-                    if file_path[0:4] == 'file':
-                        _, file_extension = os.path.splitext(file_path)
-                        if file_extension.lower() in ('.png', '.jpg', '.jpeg'):
-                            split_path = file_path.split('/./')[1].split('/')
-                            folder_name = split_path[0]
-                            image_file = split_path[1]
-                            image_name = folder_name + '/images/' + image_file
-                        elif file_extension.lower() in ('.txt', '.htm'):
-                            split_path = file_path.split('/./')[1].split('/')
-                            folder_name = split_path[0]
-                            text_file = split_path[1]
-                            with open(folder_name + '/texts/' + text_file, 'r') as file:
-                                description = file.read()
-
-            entities[topic_id] = {"label": base_name, "image": image_name, "description": description}
-
-    for association in root.findall(".//{http://www.topicmaps.org/xtm/1.0/}association"):
-        link_id = association.find(".//{http://www.topicmaps.org/xtm/1.0/}instanceOf").find(
-            "{http://www.topicmaps.org/xtm/1.0/}topicRef").attrib['{http://www.w3.org/1999/xlink}href'].split('#')[-1]
-        from_id = association.find(
-            ".//{http://www.topicmaps.org/xtm/1.0/}member[1]/{http://www.topicmaps.org/xtm/1.0/}topicRef").attrib[
-            '{http://www.w3.org/1999/xlink}href'].split('#')[-1]
-        to_id = association.find(
-            ".//{http://www.topicmaps.org/xtm/1.0/}member[2]/{http://www.topicmaps.org/xtm/1.0/}topicRef").attrib[
-            '{http://www.w3.org/1999/xlink}href'].split('#')[-1]
-        associations[link_id] = ({'from_id': from_id, 'to_id': to_id, 'label': links[link_id]["label"]})
-
-    return entities, associations
-
-
-def build_decision_tree(links):
-    decision_tree = {}
-    for id, data in links.items():
-        if data.get('from_id') not in decision_tree:
-            decision_tree[data.get('from_id')] = {}
-
-        decision_tree[data.get('from_id')][id] = data.get('to_id')
-
-    return decision_tree
-
-
-def find_root_node(entities, links):
-    target_nodes = set(data.get('to_id') for _, data in links.items())
-
-    for entity_id, _ in entities.items():
-        if entity_id not in target_nodes:
-            children_list = [{"question": {'id': item[1], 'label': entities[item[1]].get('label'),
-                                           'image': entities[item[1]].get('image')},
-                              "answer": {'id': item[0], 'label': links[item[0]].get('label'),
-                                         'image': entities[item[1]].get('image')}} for item in
-                             list(decision_tree.get(entity_id, {}).items())]
-            return {"root": {'id': entity_id, 'label': entities[entity_id].get('label'),
-                             'image': entities[entity_id].get('image'),
-                             'description': entities[entity_id].get('description')}, "children": children_list}
-
-    return None
+def clean_up_empty_directories(intermediate_dir, images_folder, texts_folder):
+    while intermediate_dir and intermediate_dir not in {images_folder, texts_folder}:
+        try:
+            os.rmdir(intermediate_dir)
+        except OSError:
+            break
+        intermediate_dir = os.path.dirname(intermediate_dir)
 
 
 if __name__ == "__main__":
